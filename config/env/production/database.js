a4f40b9f89cba388bd60b296611a2d427e281b3d
Multivalence <mulwanishiva@gmail.com>
2023-11-17 16:44:28 -0500

    revert ssl update

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -8,7 +8,7 @@ module.exports = ({ env }) => ({
         user: env('DATABASE_USERNAME'),
         password: env('DATABASE_PASSWORD'),
         ssl: {
-          rejectUnauthorized:env.bool('DATABASE_SSL_SELF', true),
+          ca: env('DATABASE_CA')
         },
       },
       debug: false,
